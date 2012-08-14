abc4eb9a3357743ff1dee3b03fc4ee990ebc4631
Feynman Liang <feynman.liang@gmail.com>
2012-08-13 19:40:51 -0700

    First comit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CashTracker::Application.config.secret_token = 'b1109cba19e58623a682bd417d314d0fc725cf76b1371d939fd3a576d5a72748a70794f5135ebbb1a217227cc8c6800428173b6e41ba6712677320984f0618a1'
