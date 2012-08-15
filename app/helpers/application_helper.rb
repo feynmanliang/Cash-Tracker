ece87b06fc3b3448bd50eb8744c1a1f3d013c983
Feynman Liang <feynman.liang@gmail.com>
2012-08-15 00:16:46 -0700

    Responsive ui

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def sign_in_out_link
     if not current_user
-      link_to "Log in with Facebook", "/auth/facebook", class: "btn btn-primary"
+      link_to "Log In", "/auth/facebook", class: "btn btn-primary"
     else
       link_to "Log Out", signout_path, class: "btn btn-primary"
     end
