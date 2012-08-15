089b14b177c19ab1a9fc4162421e98490c8aafca
Feynman Liang <feynman.liang@gmail.com>
2012-08-14 22:42:22 -0700

    Added bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def sign_in_out_link
+    if not current_user
+      link_to "Log in with Facebook", "/auth/facebook", class: "btn btn-primary"
+    else
+      link_to "Log Out", signout_path, class: "btn btn-primary"
+    end
+  end
 end
